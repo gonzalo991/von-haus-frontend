27f412ffaf20fb781c074a49fbcc607e91c02129
Gonzalo Araya <gonxaraya991@gmail.com>
2023-12-06 17:49:34 -0300

    config

--- von-haus-app/vite.config.ts
+++ von-haus-app/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/von-haus-frontend/",
+  base: "https://www.vonhauslolamora.com.ar",
 })
